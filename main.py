a8bf58c16b9576e9d186f71dfe5ab2ddc1061257
Lanoue <baptiste.lanoue@univ-lyon2.fr>
2020-10-22 20:24:31 +0200

    Commit Data
    Improve Data Window

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from PyQt5.uic.properties import QtCore
-
+from PyQt5.QtWidgets import QFileDialog
 from Front.Welcome import Ui_MainWindow
 from Front.Data_WIndow import Ui_Dialog
 import sys
@@ -12,9 +11,21 @@ def data_open():
 
 
 class Dia_Window(QtWidgets.QDialog, Ui_Dialog):
-    def __init__(self, *args, obj=None, **kwargs):
+    def __init__(self, *args, **kwargs):
         super(Dia_Window, self).__init__(*args, **kwargs)
         self.setupUi(self)
+        self.browseButton.clicked.connect(self.browse_1)
+
+    def browse_1(self):
+        file = QFileDialog()
+        filter_name = "Csv files (*.csv);;Text files (*.txt);;Xls files (*.xls);; Xlsx files (*.xlsx)"
+        file.setNameFilter(filter_name)
+        if file.exec():
+            filenames = file.selectedFiles()
+            self.browseLine.setText(str(filenames[0]))
+
+            
+
 
 
 class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
